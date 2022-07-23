109e54d88528656cb8260220ffebfd0ca2589a2e
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2022-07-23 08:01:54 -0300

    Remoção do "../"
    Imagens não carregavam

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ const games = [
     {
         nome : 'Gun',
         data : '12/2019',
-        img : '../img/banner/gun.jpg',
+        img : 'img/banner/gun.jpg',
         id : '1game',
         steam : true
     },
@@ -11,7 +11,7 @@ const games = [
     {
         nome : 'Half-Life',
         data : '03/2020',
-        img : '../img/banner/half-life.jpg',
+        img : 'img/banner/half-life.jpg',
         id : '2game',
         steam : true
     },
@@ -19,7 +19,7 @@ const games = [
     {
         nome : 'Katana ZERO',
         data : '04/2020',
-        img : '../img/banner/katana-zero.jpg',
+        img : 'img/banner/katana-zero.jpg',
         id : '3game',
         steam : true
     },
@@ -27,7 +27,7 @@ const games = [
     {
         nome : 'The Crew 2',
         data : '07/2020',
-        img : '../img/banner/the-crew-2.jpg',
+        img : 'img/banner/the-crew-2.jpg',
         id : '4game',
         steam : true
     },
@@ -35,7 +35,7 @@ const games = [
     {
         nome : 'Metal Slug 3',
         data : '08/2020',
-        img : '../img/banner/metal-slug-3.jpg',
+        img : 'img/banner/metal-slug-3.jpg',
         id : '5game',
         steam : true
     },
@@ -43,7 +43,7 @@ const games = [
     {
         nome : 'The Room',
         data : '10/2020',
-        img : '../img/banner/the-room.jpg',
+        img : 'img/banner/the-room.jpg',
         id : '6game',
         steam : true
     },
@@ -51,7 +51,7 @@ const games = [
     {
         nome : 'Portal',
         data : '10/2020',
-        img : '../img/banner/portal.jpg',
+        img : 'img/banner/portal.jpg',
         id : '7game',
         steam : true
     },
@@ -59,7 +59,7 @@ const games = [
     {
         nome : 'Resident Evil 4',
         data : '11/2020',
-        img : '../img/banner/resident-evil-4.jpg',
+        img : 'img/banner/resident-evil-4.jpg',
         id : '8game',
         steam : true
     },
@@ -67,7 +67,7 @@ const games = [
     {
         nome : 'Need For Speed',
         data : '12/2020',
-        img : '../img/banner/need-for-speed.jpg',
+        img : 'img/banner/need-for-speed.jpg',
         id : '9game',
         steam : true
     },
@@ -75,7 +75,7 @@ const games = [
     {
         nome : 'Naruto Shippuden: Ultimate Ninja STORM 4',
         data : '12/2020',
-        img : '../img/banner/naruto-shippuden-ultimate-ninja-storm-4.jpg',
+        img : 'img/banner/naruto-shippuden-ultimate-ninja-storm-4.jpg',
         id : '10game',
         steam : true
     },
@@ -83,7 +83,7 @@ const games = [
     {
         nome : 'Punch Club',
         data : '12/2020',
-        img : '../img/banner/punch-club.jpg',
+        img : 'img/banner/punch-club.jpg',
         id : '12game',
         steam : true
     },
@@ -91,7 +91,7 @@ const games = [
     {
         nome : 'Portal 2',
         data : '01/2021',
-        img : '../img/banner/portal-2.jpg',
+        img : 'img/banner/portal-2.jpg',
         id : '12game',
         steam : true
     },
@@ -99,7 +99,7 @@ const games = [
     {
         nome : 'Far Cry 4',
         data : '05/2021',
-        img : '../img/banner/far-cry-4.jpg',
+        img : 'img/banner/far-cry-4.jpg',
         id : '13game',
         steam : true
     },
@@ -107,7 +107,7 @@ const games = [
     {
         nome : 'CarX Drift Racing Online',
         data : '06/2021',
-        img : '../img/banner/carx-drift-racing-online.jpg',
+        img : 'img/banner/carx-drift-racing-online.jpg',
         id : '14game',
         steam : true
     },
@@ -115,7 +115,7 @@ const games = [
     {
         nome : 'Samurai Shodown V Special',
         data : '06/2021',
-        img : '../img/banner/samurai-shodown-v-special.jpg',
+        img : 'img/banner/samurai-shodown-v-special.jpg',
         id : '15game',
         steam : true
     },
@@ -123,7 +123,7 @@ const games = [
     {
         nome : 'Grand Theft Auto V',
         data : '09/2021',
-        img : '../img/banner/grand-theft-auto-v.jpg',
+        img : 'img/banner/grand-theft-auto-v.jpg',
         id : '16game',
         steam : true
     },
@@ -131,7 +131,7 @@ const games = [
     {
         nome : 'Please, Don`t Touch Anything',
         data : '10/2021',
-        img : '../img/banner/please-dont-touch-anything.jpg',
+        img : 'img/banner/please-dont-touch-anything.jpg',
         id : '17game',
         steam : true
     },
@@ -139,7 +139,7 @@ const games = [
     {
         nome : 'Grand Theft Auto IV',
         data : '10/2021',
-        img : '../img/banner/grand-theft-auto-iv.jpg',
+        img : 'img/banner/grand-theft-auto-iv.jpg',
         id : '18game',
         steam : true
     },
@@ -147,7 +147,7 @@ const games = [
     {
         nome : 'Sniper: Ghost Warrior',
         data : '11/2021',
-        img : '../img/banner/sniper-ghost-warrior.jpg',
+        img : 'img/banner/sniper-ghost-warrior.jpg',
         id : '19game',
         steam : true
     },
@@ -155,7 +155,7 @@ const games = [
     {
         nome : 'LEGO The Lord of the Rings',
         data : '11/2021',
-        img : '../img/banner/lego-the-lord-of-the-rings.jpg',
+        img : 'img/banner/lego-the-lord-of-the-rings.jpg',
         id : '20game',
         steam : true
     },
@@ -163,7 +163,7 @@ const games = [
     {
         nome : 'Bully: Scholarship Edition',
         data : '11/2021',
-        img : '../img/banner/bully-scholarship-edition.jpg',
+        img : 'img/banner/bully-scholarship-edition.jpg',
         id : '21game',
         steam : true
     },
@@ -171,7 +171,7 @@ const games = [
     {
         nome : 'Metal Gear Solid V: The Phanton Pain',
         data : '12/2021',
-        img : '../img/banner/metal-gear-solid-v-the-phanton-pain.jpg',
+        img : 'img/banner/metal-gear-solid-v-the-phanton-pain.jpg',
         id : '22game',
         steam : true
     },
@@ -179,7 +179,7 @@ const games = [
     {
         nome : 'Metal Gear Solid V: Ground Zeroes',
         data : '12/2021',
-        img : '../img/banner/metal-gear-solid-v-ground-zeroes.jpg',
+        img : 'img/banner/metal-gear-solid-v-ground-zeroes.jpg',
         id : '23game',
         steam : true
     },
@@ -187,7 +187,7 @@ const games = [
     {
         nome : 'Dying Light',
         data : '12/2021',
-        img : '../img/banner/dying-light.jpg',
+        img : 'img/banner/dying-light.jpg',
         id : '24game',
         steam : true
     },
@@ -195,7 +195,7 @@ const games = [
     {
         nome : 'The Elder Scrolls V: Skyrim Special Edition',
         data : '12/2021',
-        img : '../img/banner/the-elder-scrolls-v-skyrim-special-edition.jpg',
+        img : 'img/banner/the-elder-scrolls-v-skyrim-special-edition.jpg',
         id : '25game',
         steam : true
     },
@@ -203,7 +203,7 @@ const games = [
     {
         nome : 'My Hero One`s Justice',
         data : '01/2022',
-        img : '../img/banner/my-hero-ones-justice.jpg',
+        img : 'img/banner/my-hero-ones-justice.jpg',
         id : '26game',
         steam : true
     },
@@ -211,7 +211,7 @@ const games = [
     {
         nome : 'Crono Trigger',
         data : '01/2022',
-        img : '../img/banner/crono-trigger.jpg',
+        img : 'img/banner/crono-trigger.jpg',
         id : '27game',
         steam : true
     },
@@ -219,7 +219,7 @@ const games = [
     {
         nome : 'Metal Gear Rising: Revengeance',
         data : '02/2022',
-        img : '../img/banner/metal-gear-rising-revengeance.jpg',
+        img : 'img/banner/metal-gear-rising-revengeance.jpg',
         id : '28game',
         steam : true
     },
@@ -227,7 +227,7 @@ const games = [
     {
         nome : 'God of War',
         data : '02/2022',
-        img : '../img/banner/god-of-war.jpg',
+        img : 'img/banner/god-of-war.jpg',
         id : '29game',
         steam : true
     },
@@ -235,7 +235,7 @@ const games = [
     {
         nome : 'Resident Evil 2',
         data : '07/2022',
-        img : '../img/banner/resident-evil-2.jpg',
+        img : 'img/banner/resident-evil-2.jpg',
         id : '30game',
         steam : true
     },
@@ -243,7 +243,7 @@ const games = [
     {
         nome : 'Skate 3',
         data : '03/2022',
-        img : '../img/banner/skate-3.jpg',
+        img : 'img/banner/skate-3.jpg',
         id : '31game',
         steam : false
     },
@@ -251,7 +251,7 @@ const games = [
     {
         nome : 'Guitar Hero III: Legends of Rock',
         data : '06/2022',
-        img : '../img/banner/guitar-hero-3-legends-of-rock.jpg',
+        img : 'img/banner/guitar-hero-3-legends-of-rock.jpg',
         id : '32game',
         steam : false
     },
@@ -259,7 +259,7 @@ const games = [
     {
         nome : 'Minecraft',
         data : '07/2022',
-        img : '../img/banner/minecraft.jpg',
+        img : 'img/banner/minecraft.jpg',
         id : '33game',
         steam : false
     }
