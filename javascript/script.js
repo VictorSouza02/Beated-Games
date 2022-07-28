031374a2306c5de0f755f501ec9c42cc6ea83d72
Souza Victor <victorbatistasouza2002@gmail.com>
2022-07-28 20:52:50 -0300

    Hover adicionado
      Adicionado Hover nas "cartas" para ver a descrição do jogo.
      Descrição sobre os jogos e dados ao finalizar não adicionados.

--- javascript/script.js
+++ javascript/script.js
@@ -3,265 +3,364 @@ const games = [
     {
         nome : 'Gun',
         data : '12/2019',
-        img : 'img/banner/gun.jpg',
+        banner : 'img/games/banner/gun.jpg',
+        bannerBlur : 'img/games/banner/blur/gun.jpg',
+        header : 'img/games/header/gun.jpg',
+        icone : 'img/games/icon/gun.jpg',
         id : '1game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Half-Life',
         data : '03/2020',
-        img : 'img/banner/half-life.jpg',
+        banner : 'img/games/banner/half-life.jpg',
+        bannerBlur : 'img/games/banner/blur/half-life.jpg',
+        header : 'img/games/header/half-life.jpg',
+        icone : 'img/games/icon/half-life.jpg',
         id : '2game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Katana ZERO',
         data : '04/2020',
-        img : 'img/banner/katana-zero.jpg',
+        banner : 'img/games/banner/katana-zero.jpg',
+        bannerBlur : 'img/games/banner/blur/katana-zero.jpg',
+        header : 'img/games/header/katana-zero.jpg',
+        icone : 'img/games/icon/katana-zero.jpg',
         id : '3game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'The Crew 2',
         data : '07/2020',
-        img : 'img/banner/the-crew-2.jpg',
+        banner : 'img/games/banner/the-crew-2.jpg',
+        bannerBlur : 'img/games/banner/blur/the-crew-2.jpg',
+        header : 'img/games/header/the-crew-2.jpg',
+        icone : 'img/games/icon/the-crew-2.jpg',
         id : '4game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Metal Slug 3',
         data : '08/2020',
-        img : 'img/banner/metal-slug-3.jpg',
+        banner : 'img/games/banner/metal-slug-3.jpg',
+        bannerBlur : 'img/games/banner/blur/metal-slug-3.jpg',
+        header : 'img/games/header/metal-slug-3.jpg',
+        icone : 'img/games/icon/metal-slug-3.jpg',
         id : '5game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'The Room',
         data : '10/2020',
-        img : 'img/banner/the-room.jpg',
+        banner : 'img/games/banner/the-room.jpg',
+        bannerBlur : 'img/games/banner/blur/the-room.jpg',
+        header : 'img/games/header/the-room.jpg',
+        icone : 'img/games/icon/the-room.jpg',
         id : '6game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Portal',
         data : '10/2020',
-        img : 'img/banner/portal.jpg',
+        banner : 'img/games/banner/portal.jpg',
+        bannerBlur : 'img/games/banner/blur/portal.jpg',
+        header : 'img/games/header/portal.jpg',
+        icone : 'img/games/icon/portal.jpg',
         id : '7game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Resident Evil 4',
         data : '11/2020',
-        img : 'img/banner/resident-evil-4.jpg',
+        banner : 'img/games/banner/resident-evil-4.jpg',
+        bannerBlur : 'img/games/banner/blur/resident-evil-4.jpg',
+        header : 'img/games/header/resident-evil-4.jpg',
+        icone : 'img/games/icon/resident-evil-4.jpg',
         id : '8game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Need For Speed',
         data : '12/2020',
-        img : 'img/banner/need-for-speed.jpg',
+        banner : 'img/games/banner/need-for-speed.jpg',
+        bannerBlur : 'img/games/banner/blur/need-for-speed.jpg',
+        header : 'img/games/header/need-for-speed.jpg',
+        icone : 'img/games/icon/need-for-speed.jpg',
         id : '9game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Naruto Shippuden: Ultimate Ninja STORM 4',
         data : '12/2020',
-        img : 'img/banner/naruto-shippuden-ultimate-ninja-storm-4.jpg',
+        banner : 'img/games/banner/naruto-shippuden-ultimate-ninja-storm-4.jpg',
+        bannerBlur : 'img/games/banner/blur/naruto-shippuden-ultimate-ninja-storm-4.jpg',
+        header : 'img/games/header/naruto-shippuden-ultimate-ninja-storm-4.jpg',
+        icone : 'img/games/icon/naruto-shippuden-ultimate-ninja-storm-4.jpg',
         id : '10game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Punch Club',
         data : '12/2020',
-        img : 'img/banner/punch-club.jpg',
+        banner : 'img/games/banner/punch-club.jpg',
+        bannerBlur : 'img/games/banner/blur/punch-club.jpg',
+        header : 'img/games/header/punch-club.jpg',
+        icone : 'img/games/icon/punch-club.jpg',
         id : '12game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Portal 2',
         data : '01/2021',
-        img : 'img/banner/portal-2.jpg',
+        banner : 'img/games/banner/portal-2.jpg',
+        bannerBlur : 'img/games/banner/blur/portal-2.jpg',
+        header : 'img/games/header/portal-2.jpg',
+        icone : 'img/games/icon/portal-2.jpg',
         id : '12game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Far Cry 4',
         data : '05/2021',
-        img : 'img/banner/far-cry-4.jpg',
+        banner : 'img/games/banner/far-cry-4.jpg',
+        bannerBlur : 'img/games/banner/blur/far-cry-4.jpg',
+        header : 'img/games/header/far-cry-4.jpg',
+        icone : 'img/games/icon/far-cry-4.jpg',
         id : '13game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'CarX Drift Racing Online',
         data : '06/2021',
-        img : 'img/banner/carx-drift-racing-online.jpg',
+        banner : 'img/games/banner/carx-drift-racing-online.jpg',
+        bannerBlur : 'img/games/banner/blur/carx-drift-racing-online.jpg',
+        header : 'img/games/header/carx-drift-racing-online.jpg',
+        icone : 'img/games/icon/carx-drift-racing-online.jpg',
         id : '14game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Samurai Shodown V Special',
         data : '06/2021',
-        img : 'img/banner/samurai-shodown-v-special.jpg',
+        banner : 'img/games/banner/samurai-shodown-v-special.jpg',
+        bannerBlur : 'img/games/banner/blur/samurai-shodown-v-special.jpg',
+        header : 'img/games/header/samurai-shodown-v-special.jpg',
+        icone : 'img/games/icon/samurai-shodown-v-special.jpg',
         id : '15game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Grand Theft Auto V',
         data : '09/2021',
-        img : 'img/banner/grand-theft-auto-v.jpg',
+        banner : 'img/games/banner/grand-theft-auto-v.jpg',
+        bannerBlur : 'img/games/banner/blur/grand-theft-auto-v.jpg',
+        header : 'img/games/header/grand-theft-auto-v.jpg',
+        icone : 'img/games/icon/grand-theft-auto-v.jpg',
         id : '16game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Please, Don`t Touch Anything',
         data : '10/2021',
-        img : 'img/banner/please-dont-touch-anything.jpg',
+        banner : 'img/games/banner/please-dont-touch-anything.jpg',
+        bannerBlur : 'img/games/banner/blur/please-dont-touch-anything.jpg',
+        header : 'img/games/header/please-dont-touch-anything.jpg',
+        icone : 'img/games/icon/please-dont-touch-anything.jpg',
         id : '17game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Grand Theft Auto IV',
         data : '10/2021',
-        img : 'img/banner/grand-theft-auto-iv.jpg',
+        banner : 'img/games/banner/grand-theft-auto-iv.jpg',
+        bannerBlur : 'img/games/banner/blur/grand-theft-auto-iv.jpg',
+        header : 'img/games/header/grand-theft-auto-v.jpg',
+        icone : 'img/games/icon/grand-theft-auto-iv.jpg',
         id : '18game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Sniper: Ghost Warrior',
         data : '11/2021',
-        img : 'img/banner/sniper-ghost-warrior.jpg',
+        banner : 'img/games/banner/sniper-ghost-warrior.jpg',
+        bannerBlur : 'img/games/banner/blur/sniper-ghost-warrior.jpg',
+        header : 'img/games/header/sniper-ghost-warrior.jpg',
+        icone : 'img/games/icon/sniper-ghost-warrior.jpg',
         id : '19game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'LEGO The Lord of the Rings',
         data : '11/2021',
-        img : 'img/banner/lego-the-lord-of-the-rings.jpg',
+        banner : 'img/games/banner/lego-the-lord-of-the-rings.jpg',
+        bannerBlur : 'img/games/banner/blur/lego-the-lord-of-the-rings.jpg',
+        header : 'img/games/header/lego-the-lord-of-the-rings.jpg',
+        icone : 'img/games/icon/lego-the-lord-of-the-rings.jpg',
         id : '20game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Bully: Scholarship Edition',
         data : '11/2021',
-        img : 'img/banner/bully-scholarship-edition.jpg',
+        banner : 'img/games/banner/bully-scholarship-edition.jpg',
+        bannerBlur : 'img/games/banner/blur/bully-scholarship-edition.jpg',
+        header : 'img/games/header/bully-scholarship-edition.jpg',
+        icone : 'img/games/icon/bully-scholarship-edition.jpg',
         id : '21game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Metal Gear Solid V: The Phanton Pain',
         data : '12/2021',
-        img : 'img/banner/metal-gear-solid-v-the-phanton-pain.jpg',
+        banner : 'img/games/banner/metal-gear-solid-v-the-phanton-pain.jpg',
+        bannerBlur : 'img/games/banner/blur/metal-gear-solid-v-the-phanton-pain.jpg',
+        header : 'img/games/header/metal-gear-solid-v-the-phanton-pain.jpg',
+        icone : 'img/games/icon/metal-gear-solid-v-the-phanton-pain.jpg',
         id : '22game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Metal Gear Solid V: Ground Zeroes',
         data : '12/2021',
-        img : 'img/banner/metal-gear-solid-v-ground-zeroes.jpg',
+        banner : 'img/games/banner/metal-gear-solid-v-ground-zeroes.jpg',
+        bannerBlur : 'img/games/banner/blur/metal-gear-solid-v-ground-zeroes.jpg',
+        header : 'img/games/header/metal-gear-solid-v-ground-zeroes.jpg',
+        icone : 'img/games/icon/metal-gear-solid-v-ground-zeroes.jpg',
         id : '23game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Dying Light',
         data : '12/2021',
-        img : 'img/banner/dying-light.jpg',
+        banner : 'img/games/banner/dying-light.jpg',
+        bannerBlur : 'img/games/banner/blur/dying-light.jpg',
+        header : 'img/games/header/dying-light.jpg',
+        icone : 'img/games/icon/dying-light.jpg',
         id : '24game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'The Elder Scrolls V: Skyrim Special Edition',
         data : '12/2021',
-        img : 'img/banner/the-elder-scrolls-v-skyrim-special-edition.jpg',
+        banner : 'img/games/banner/the-elder-scrolls-v-skyrim-special-edition.jpg',
+        bannerBlur : 'img/games/banner/blur/the-elder-scrolls-v-skyrim-special-edition.jpg',
+        header : 'img/games/header/the-elder-scrolls-v-skyrim-special-edition.jpg',
+        icone : 'img/games/icon/the-elder-scrolls-v-skyrim-special-edition.jpg',
         id : '25game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'My Hero One`s Justice',
         data : '01/2022',
-        img : 'img/banner/my-hero-ones-justice.jpg',
+        banner : 'img/games/banner/my-hero-ones-justice.jpg',
+        bannerBlur : 'img/games/banner/blur/my-hero-ones-justice.jpg',
+        header : 'img/games/header/my-hero-ones-justice.jpg',
+        icone : 'img/games/icon/my-hero-ones-justice.jpg',
         id : '26game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Crono Trigger',
         data : '01/2022',
-        img : 'img/banner/crono-trigger.jpg',
+        banner : 'img/games/banner/crono-trigger.jpg',
+        bannerBlur : 'img/games/banner/blur/crono-trigger.jpg',
+        header : 'img/games/header/crono-trigger.jpg',
+        icone : 'img/games/icon/crono-trigger.jpg',
         id : '27game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Metal Gear Rising: Revengeance',
         data : '02/2022',
-        img : 'img/banner/metal-gear-rising-revengeance.jpg',
+        banner : 'img/games/banner/metal-gear-rising-revengeance.jpg',
+        bannerBlur : 'img/games/banner/blur/metal-gear-rising-revengeance.jpg',
+        header : 'img/games/header/metal-gear-rising-revengeance.jpg',
+        icone : 'img/games/icon/metal-gear-rising-revengeance.jpg',
         id : '28game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'God of War',
         data : '02/2022',
-        img : 'img/banner/god-of-war.jpg',
+        banner : 'img/games/banner/god-of-war.jpg',
+        bannerBlur : 'img/games/banner/blur/god-of-war.jpg',
+        header : 'img/games/header/god-of-war.jpg',
+        icone : 'img/games/icon/god-of-war.jpg',
         id : '29game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Resident Evil 2',
         data : '07/2022',
-        img : 'img/banner/resident-evil-2.jpg',
+        banner : 'img/games/banner/resident-evil-2.jpg',
+        bannerBlur : 'img/games/banner/blur/resident-evil-2.jpg',
+        header : 'img/games/header/resident-evil-2.jpg',
+        icone : 'img/games/icon/resident-evil-2.jpg',
         id : '30game',
-        steam : true
+        steam : true,
     },
 
     {
         nome : 'Skate 3',
         data : '03/2022',
-        img : 'img/banner/skate-3.jpg',
+        banner : 'img/games/banner/skate-3.jpg',
+        bannerBlur : 'img/games/banner/blur/skate-3.jpg',
+        header : 'img/games/header/skate-3.jpg',
+        icone : 'img/games/icon/skate-3.png',
         id : '31game',
-        steam : false
+        steam : false,
     },
 
     {
         nome : 'Guitar Hero III: Legends of Rock',
         data : '06/2022',
-        img : 'img/banner/guitar-hero-3-legends-of-rock.jpg',
+        banner : 'img/games/banner/guitar-hero-3-legends-of-rock.jpg',
+        bannerBlur : 'img/games/banner/blur/guitar-hero-3-legends-of-rock.jpg',
+        header : 'img/games/header/guitar-hero-3-legends-of-rock.jpg',
+        icone : 'img/games/icon/guitar-hero-3-legends-of-rock.png',
         id : '32game',
-        steam : false
+        steam : false,
     },
 
     {
         nome : 'Minecraft',
         data : '07/2022',
-        img : 'img/banner/minecraft.jpg',
+        banner : 'img/games/banner/minecraft.jpg',
+        bannerBlur : 'img/games/banner/blur/minecraft.jpg',
+        header : 'img/games/header/minecraft.jpg',
+        icone : 'img/games/icon/minecraft.png',
         id : '33game',
-        steam : false
+        steam : false,
     }
 ]
 
@@ -273,19 +372,55 @@ inicializarGrid = () => {
     games.map((val) => {
         if(val.steam===true){
             adicionarItens.innerHTML+=`
-            <div id='`+val.id+`'  
-            class='item' 
-            style='background-image: url(`+val.img+`);'>
+            <div id="`+val.id+`" class="item">
+                <div class="carta">
+                    <div class="carta-frente" style="background-image: url(`+val.banner+`);"></div>
+                        
+                    <div class="carta-costas" style="background-image: url(`+val.bannerBlur+`);">
+                        <div class="carta-costas-descricao">
+                            <img src="`+val.icone+`">
+                            <h2>`+val.nome+`</h2> 
+                            <p>Produtora</p>
+                            <p>Data de lançamento</p>
+
+                            <div class="carta-costas-beated">
+                                <img src="img/icons/steam.png">
+                                <h2>Beated Game</h2> 
+                                <p><s>`+val.data+`</s></p>
+                                <p>Horas de jogo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        `;
-        }else{
+            `;
+        }
+        
+        else{
             const adicionarNonSteam = document.getElementById('grid-non-steam')
             adicionarNonSteam.innerHTML+=`
-            <div id='`+val.id+`'  
-            class='item' 
-            style='background-image: url(`+val.img+`);'>
+            <div id="`+val.id+`" class="item">
+                <div class="carta">
+                    <div class="carta-frente" style="background-image: url(`+val.banner+`);"></div>
+                        
+                    <div class="carta-costas" style="background-image: url(`+val.bannerBlur+`);">
+                        <div class="carta-costas-descricao">
+                            <img src="`+val.icone+`">
+                            <h2>`+val.nome+`</h2> 
+                            <p>Produtora</p>
+                            <p>Data de lançamento</p>
+
+                            <div class="carta-costas-beated">
+                                <img src="`+val.icone+`">
+                                <h2>Beated Game</h2> 
+                                <p><s>`+val.data+`</s></p>
+                                <p>Horas de jogo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        `;
+            `;
             
 
         }
