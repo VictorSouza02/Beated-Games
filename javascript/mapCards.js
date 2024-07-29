677d1191908155a7fd85841485c69593f0f08759
Víctor Souza <victorbatistasouza2002@gmail.com>
2024-07-29 10:38:34 -0300

    :bug: fix: fixed map cards and mortal shell added

--- javascript/mapCards.js
+++ javascript/mapCards.js
@@ -1,45 +1,40 @@
-/* JSON */
-import lista from './games-list.json' assert {type: 'json'}
+const loadJSON = async () => {
+  try {
+    const response = await fetch('./javascript/games-list.json');
+    if (!response.ok) throw new Error('Failed to load JSON');
 
-// Ordenar a lista de jogos com base na data de conclusão (beatInfo)
-lista.sort((a, b) => {
-  // Convertendo as datas para objetos Date para facilitar a comparação
-  const dateA = new Date(a.beatInfo[0].date);
-  const dateB = new Date(b.beatInfo[0].date);
+    const gamesList = await response.json();
+    gamesList.sort((a, b) => new Date(b.beatInfo[0].date) - new Date(a.beatInfo[0].date));
 
-  // Ordenando de forma decrescente
-  return dateB - dateA;
-});
+    initializeGrid(gamesList);
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+  }
+};
 
-/* Fim JSON */
-
-/* Adicionar itens */
-const inicializarGrid = () => {
-    const adicionarItens = document.getElementById('container')
-    lista.map((val) => {
-      adicionarItens.innerHTML += `
-        <div id="`+ val['id'] + `" class="game-card">
-          <div class="internal-card">
-            <div class="front-side" style="background-image: url(`+ val['photos'][0]['banner'] + `);"></div>
-                            
-            <div class="back-side" style="background-image: url(`+ val['photos'][0]['bannerBlur'] + `);">
-              <div class="back-side-description">
-                <img src="`+ val['photos'][0]['icon'] + `">
-                <h2 class="text-bg">`+ val['name'] + `</h2> 
-                <p class="text-bg">`+ val['developer'] + `<br>` + val['releaseDate'] + `</p>
-
-                <div class="back-side-beated">
-                  <h2>Beated Game</h2> 
-                  <p>`+ val['beatInfo'][0]['date'] + `</p>
-                  <p>`+ val['beatInfo'][0]['hours'] + ` hours</p>
-                </div>
+const initializeGrid = (gamesList) => {
+  const container = document.getElementById('container');
+  gamesList.forEach((game) => {
+    container.innerHTML += `
+      <div id="${game.id}" class="game-card">
+        <div class="internal-card">
+          <div class="front-side" style="background-image: url(${game.photos[0].banner});"></div>
+          <div class="back-side" style="background-image: url(${game.photos[0].bannerBlur});">
+            <div class="back-side-description">
+              <img src="${game.photos[0].icon}">
+              <h2 class="text-bg">${game.name}</h2> 
+              <p class="text-bg">${game.developer}<br>${game.releaseDate}</p>
+              <div class="back-side-beated">
+                <h2>Beated Game</h2> 
+                <p>${game.beatInfo[0].date}</p>
+                <p>${game.beatInfo[0].hours} hours</p>
               </div>
             </div>
           </div>
         </div>
-      `;
-    })
-}
+      </div>
+    `;
+  });
+};
 
-inicializarGrid()
-/* FIM Adicionar itens */
+loadJSON();
